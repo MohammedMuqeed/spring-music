9408c3ee306ceec081899722fd6c0d41bd733cb5
Abdul Muqeed Mohammed <abdulmuqeed@Abduls-MacBook-Pro.local>
2020-12-11 19:28:12 -0800

    Converting jar into war file

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -4,9 +4,10 @@ import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer
 import org.cloudfoundry.samples.music.repositories.AlbumRepositoryPopulator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
